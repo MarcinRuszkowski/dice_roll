27a62c2aec127c63d2be0dabc207d1a4323358fa
MarcinRuszkowski <marcin.ruszkowski01@gmail.com>
2023-10-26 20:15:13 +0200

    turn code into 3 func

--- dice_roll.py
+++ dice_roll.py
@@ -32,10 +32,10 @@ dice_art = {
         "│  ●   ●  │",
         "└─────────┘")
 }
-
+'''
 diceRolls = []
 
-amountOfRolls = 2
+amountOfRolls = int(input('How many rolls? '))
 for dice in range(amountOfRolls):
     diceRolls.append(randint(1, 6))
 
@@ -47,7 +47,30 @@ rolledValues = 0
 for num in diceRolls:
     rolledValues += num
 
-print(rolledValues)
+print(f'Rolled values: {rolledValues}')
+'''
+# func
+
+def roll_dice(amountOfRoll):
+    diceRolls = []
+    for dice in range(amountOfRoll):
+        diceRolls.append(randint(1, 6))
+
+    return diceRolls
+
+def count_rolled_values(diceRolls):
+    rolledValues = 0
+    for value in diceRolls:
+        rolledValues += value
+    print(rolledValues)
 
 
+def show_rolled_dice(diceRolls):
+    for num in diceRolls:
+        for line in dice_art.get(num):
+            print(line)
 
+
+diceRolls = roll_dice(3)
+show_rolled_dice(diceRolls)
+count_rolled_values(diceRolls)
